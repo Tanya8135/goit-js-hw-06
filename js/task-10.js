cdaf0258e5b4ab8194c4eb1e7e7feab320e20d53
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-03-16 14:53:26 +0200

    solutions task: 8, 9, 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,34 @@
+const controls = document.querySelector('#controls');
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    const color = getRandomHexColor();
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = color;
+    boxes.appendChild(box);
+    boxSize += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+createBtn.addEventListener('click', () => {
+  const amount = +controls.querySelector('input').value;
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
